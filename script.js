aff37910c38b0fc8adca309cc54b71a1cc118015
SylvainK200 <72570934+SylvainK200@users.noreply.github.com>
2021-04-18 21:52:53 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 function compute()
 {
     amount = parseInt(document.getElementById("principal").value);
+    if (amount>0){   
     rate = parseFloat(document.getElementById("rate").value);
-    years =parseInt(document.getElementById("years").value);
-    let receive = amount;
-    var d= new Date();
-    dateFinal = parseInt(d.getFullYear())+years;
-    for (i=1;i<=years;i++){
-        receive=receive*((rate+100)/100);
+        years =parseInt(document.getElementById("years").value);
+        let receive = amount;
+        var d= new Date();
+        dateFinal = parseInt(d.getFullYear())+years;
+        for (i=1;i<=years;i++){
+            receive=receive*((rate+100)/100);
+        }
+        receive=parseInt(receive)
+        p= document.getElementById("result");
+        p.innerHTML="<p>If you deposit <span> "+amount+",</span><br>"+
+                    "at an interest rate of <span> "+rate+"%.</span><br>"+
+                    "You win receive an amount of <span>"+receive+" </span>,<br>"+
+                    "in the year <span>"+dateFinal+"</span></p>";
     }
-    receive=parseInt(receive)
-    p= document.getElementById("result");
-    p.innerHTML="<p>If you deposit <span> "+amount+",</span><br>"+
-                "at an interest rate of <span> "+rate+"%.</span><br>"+
-                "You win receive an amount of <span>"+receive+" </span>,<br>"+
-                "in the year <span>"+dateFinal+"</span></p>";
+    else{
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
 }
 function changeValue(val){
    p=document.getElementById("value");
